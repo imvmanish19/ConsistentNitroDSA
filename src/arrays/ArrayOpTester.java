26a33337942b75006383c4b0ddc5387af92c6103
Manish V <imvmanish19@gmail.com>
2023-05-15 11:50:57 +0530

    Added Array Operations Files

---
+++ src/arrays/ArrayOpTester.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class ArrayOpTester {
+    public static void main(String[] args) {
+        ArrayOperations arrayOperations = new ArrayOperations(10);
+        arrayOperations.initializeWithRandomValues(5);
+        arrayOperations.printElements();
+        arrayOperations.insert(5,1000);
+        arrayOperations.printElements();
+        System.out.println(arrayOperations.length);
+        arrayOperations.delete(2);
+        arrayOperations.delete(4);
+        arrayOperations.printElements();
+    }
+}
---
+++ src/arrays/LinearSearch.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int numbers[] = {10,20,30,-40,50,-60};
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        System.out.println(linearSearch(numbers, target));
+    }
+
+    private static int linearSearch(int numbers[], int target) {
+        for(int i=0;i<numbers.length;i++) {
+            if(numbers[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
