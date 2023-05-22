fd9a08f369617c22d82578a5b6fa887b2a26b6e9
Manish V <imvmanish19@gmail.com>
2023-05-22 16:24:36 +0530

    Added Double Linked List Code

---
+++ src/linkedList/doubly/Node.java
@@ -0,0 +1,17 @@
+package linkedList.doubly;
+
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public Node(int val, Node nextNode, Node prevNode) {
+        this.val = val;
+        this.next = nextNode;
+        this.prev = prevNode;
+    }
+}
