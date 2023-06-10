14cb9c646931d6576bb950d20c9563cd127c0f1d
Manish V <imvmanish19@gmail.com>
2023-06-10 14:07:12 +0530

    Added bubble sort

--- src/sorting/simple/BubbleSort.java
+++ src/sorting/simple/BubbleSort.java
@@ -1,29 +1,29 @@
 package sorting.simple;
 
+import recursion.basics.InputTaker;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[] = {12,7,100,2,9,1};
+        int[] arr = {2,4,1,0,5,0,1};
         bubbleSort(arr);
-        printArr(arr);
+        InputTaker.printArray(arr);
     }
 
     private static void bubbleSort(int[] arr) {
         int n = arr.length;
-        for(int i=n-1;i>=0;i--) {
-            for(int j=0;j<i;j++) {
+        for(int i=0;i<n-1;i++) {
+            boolean isSwapped = false;
+            for(int j=0;j<n-1-i;j++) {
                 if(arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    isSwapped = true;
                 }
             }
+            if(!isSwapped) {
+                break;
+            }
         }
-    }
-
-    private static void printArr(int[] arr) {
-        for(int x: arr) {
-            System.out.print(x+" ");
-        }
-        System.out.println();
     }
 }
